0cb5a83f22db8da4bed1b8a88ecf12a96325f329
level237 <Bramslevel129@gmail.com>
2024-12-15 15:58:41 +0100

    updat

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -27,7 +27,7 @@ export default function DashboardPage() {
                 </div>
             </div>
             <div>
-      <Link to="/">
+      <Link to="#">
             
             <a className="text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Retrait en USDT</a>
             </Link>
