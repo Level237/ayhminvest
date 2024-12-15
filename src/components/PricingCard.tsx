2a3563b61ef069f2f54e1384ec8dd6dcf86ab9f6
level237 <Bramslevel129@gmail.com>
2024-12-15 16:09:08 +0100

    updat

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -7,8 +7,8 @@ export default function PricingCard() {
     <section className="">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">Designed for business teams like yours</h2>
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">Listes des paliers</h2>
+          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at AYHMINVEST we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
       </div>
       <div className="space-y-8 lg:grid lg:grid-cols-5 sm:gap-6 xl:gap-4 lg:space-y-0">
        
